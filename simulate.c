8029f3870d91f18c8e672ed12f013e11de08d310
Sean Siddens <seansiddens@gmail.com>
2021-06-27 10:26:03 -0700

    Implemented fetch_instruction()

--- simulate.c
+++ simulate.c
@@ -3,9 +3,15 @@
 #include <stdio.h>
 
 #define MEMORY_SIZE 4294967296
+// ----------------------------------------------------------------------------
 // Memory layout
+// ----------------------------------------------------------------------------
 #define PRGM_TXT_START 0x00400000
 #define PRGM_TXT_END 0x0fffffff
+
+// ----------------------------------------------------------------------------
+// Utility functions
+// ----------------------------------------------------------------------------
 
 // Memory
 uint8_t MEM[MEMORY_SIZE] = {0};
@@ -49,22 +55,40 @@ enum register_names {
 // Register file
 uint32_t REGS[32] = {0};
 
-// Program counter
-uint32_t PC = 0;
-
-uint32_t fetch() {
-
+//
+// Fetches the machine code instruction pointed to by the program counter.
+//
+// Data is layed out in little endian byte order: 
+// 4|        | <-- PC + 4 (next instruction)
+// 3|  MSBs  |
+// 2|xxxxxxxx|   
+// 1|xxxxxxxx|   
+// 0|  LSBs  | <-- PC
+//
+static inline uint32_t fetch_instruction(uint32_t PC) {
+    // Instructions are 32 bits wide, but the memory is byte addressable. 
+    // Casting the u8 pointer into memory to u32 fetches the 4 bytes we need.
+    return *((uint32_t *)(MEM + PC));
 }
 
-int main(void) { 
-    // Instruction cycle: 
+int main(void) {
+    // Instruction cycle:
     // 1. Fetch instruction from memory
     // 2. Decode instruction into it's corresponding opcode
     // 3. Execute instruction.
-    // 4. Repeat
+    // 4. Increment PC to point to next instruction in memory
 
+    // Initialize program counter to point to beginning of text segment
+    uint32_t PC = PRGM_TXT_START;
 
+    MEM[PRGM_TXT_START] = 0xff;
+    MEM[PRGM_TXT_START + 1] = 0xa5;
+    MEM[PRGM_TXT_START + 2] = 0xb7;
+    MEM[PRGM_TXT_START + 3] = 0x98;
 
-    return 0; 
+    uint32_t instruction = fetch_instruction(PC);
+
+    printf("%x\n", instruction);
+
+    return 0;
 }
-
